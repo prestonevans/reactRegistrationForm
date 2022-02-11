e10ed6b0a0c8bfe7e9dd8f9c3ef3da01a227ed14
Preston Evans <preston.evans3074@stu.mtec.edu>
2022-02-10 22:18:30 -0700

    added regex to password input

--- src/components/form/Form.component.tsx
+++ src/components/form/Form.component.tsx
@@ -1,23 +1,32 @@
 import { ChangeEvent } from 'react';
-import './Form.css'
+import './Form.css';
 
 export default function Form() {
-    function submitHandler(e:ChangeEvent<HTMLFormElement>) {
-        e.preventDefault();
-        alert('Form submitted')
-    }
+	function submitHandler(e: ChangeEvent<HTMLFormElement>) {
+		e.preventDefault();
+		alert('Form submitted');
+	}
 
 	return (
 		<form onSubmit={submitHandler} className="form" action="https://fcc-registration-form.com">
 			<fieldset>
-				<label>Enter Your First Name: <input required type="text" name="firstName" /></label>
-				<label>Enter Your Last Name: <input required type="text" name="lastName" /></label>
-				<label>Enter Your Email: <input required type="email" name="email" /></label>
-				<label>Create a New Password: <input required type="password" name="password" /></label>
+				<label>
+					Enter Your First Name: <input required type="text" name="firstName" />
+				</label>
+				<label>
+					Enter Your Last Name: <input required type="text" name="lastName" />
+				</label>
+				<label>
+					Enter Your Email: <input required type="email" name="email" />
+				</label>
+				<label title='Minimum eight characters, at least one letter, one number and one special character'>
+					Create a New Password:
+					<input required type="password" name="password" pattern='^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$' />
+				</label>
 			</fieldset>
 			<fieldset />
 			<fieldset />
-            <input type='submit' value='Submit'></input>
+			<input type="submit" value="Submit" />
 		</form>
 	);
 }
